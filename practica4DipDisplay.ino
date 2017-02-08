2442d7744326671d4b707cfd06af59c155c5f554
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2017-02-08 14:05:06 -0600

    Optimización de Código
    eliminamos las comparaciones de los "if" y modificamos la funcion display precismente en los digitalWrite.

--- practica4DipDisplay.ino
+++ practica4DipDisplay.ino
@@ -31,7 +31,7 @@ void setup() {
    void display (int d1,int d2,int d3,int d4,int d5,int d6,int d7,int numero)// Función del display
   {
     int a, b, c, d, e, f, g;
-    byte  Digit[10][7] =
+    byte  Digit[11][7] =
     {
        { 1,1,1,1,1,1,0 },    // 0
        { 0,1,1,0,0,0,0 },    // 1
@@ -42,45 +42,27 @@ void setup() {
        { 1,0,1,1,1,1,1 },    // 6
        { 1,1,1,0,0,0,0 },    // 7
        { 1,1,1,1,1,1,1 },    // 8
-       { 1,1,1,0,0,1,1 }     // 9
+       { 1,1,1,0,0,1,1 },    // 9
+       { 0,0,0,0,0,0,0 }     //null
     };
-    if (numero==0){
-      a=Digit[0][0];b=Digit[0][1];c=Digit[0][2];d=Digit[0][3];e=Digit[0][4];f=Digit[0][5];g=Digit[0][6];
+    if (numero<=9){
+  digitalWrite (d1,Digit[numero][0]);
+  digitalWrite (d2,Digit[numero][1]);
+  digitalWrite (d3,Digit[numero][2]);
+  digitalWrite (d4,Digit[numero][3]);
+  digitalWrite (d5,Digit[numero][4]);
+  digitalWrite (d6,Digit[numero][5]);
+  digitalWrite (d7,Digit[numero][6]);
     }
-     if (numero==1){
-      a=Digit[1][0];b=Digit[1][1];c=Digit[1][2];d=Digit[1][3];e=Digit[1][4];f=Digit[1][5];g=Digit[1][6];
-    }
-     if (numero==2){
-      a=Digit[2][0];b=Digit[2][1];c=Digit[2][2];d=Digit[2][3];e=Digit[2][4];f=Digit[2][5];g=Digit[2][6];
-    }
-     if (numero==3){
-      a=Digit[3][0];b=Digit[3][1];c=Digit[3][2];d=Digit[3][3];e=Digit[3][4];f=Digit[3][5];g=Digit[3][6];
-    }
-     if (numero==4){
-      a=Digit[4][0];b=Digit[4][1];c=Digit[4][2];d=Digit[4][3];e=Digit[4][4];f=Digit[4][5];g=Digit[4][6];
-    }
-     if (numero==5){
-      a=Digit[5][0];b=Digit[5][1];c=Digit[5][2];d=Digit[5][3];e=Digit[5][4];f=Digit[5][5];g=Digit[5][6];
-    }
-     if (numero==6){
-      a=Digit[6][0];b=Digit[6][1];c=Digit[6][2];d=Digit[6][3];e=Digit[6][4];f=Digit[6][5];g=Digit[6][6];
-    }
-     if (numero==7){
-      a=Digit[7][0];b=Digit[7][1];c=Digit[7][2];d=Digit[7][3];e=Digit[7][4];f=Digit[7][5];g=Digit[7][6];
-    }
-     if (numero==8){
-      a=Digit[8][0];b=Digit[8][1];c=Digit[8][2];d=Digit[8][3];e=Digit[8][4];f=Digit[8][5];g=Digit[8][6];
-    }
-     if (numero==9){
-      a=Digit[9][0];b=Digit[9][1];c=Digit[9][2];d=Digit[9][3];e=Digit[9][4];f=Digit[9][5];g=Digit[9][6];
-    }
-  digitalWrite (d1,a);
-  digitalWrite (d2,b);
-  digitalWrite (d3,c);
-  digitalWrite (d4,d);
-  digitalWrite (d5,e);
-  digitalWrite (d6,f);
-  digitalWrite (d7,g);
+    else if(numero>=10){
+      digitalWrite (d1,Digit[10][0]);
+      digitalWrite (d2,Digit[10][1]);
+      digitalWrite (d3,Digit[10][2]);
+      digitalWrite (d4,Digit[10][3]);
+      digitalWrite (d5,Digit[10][4]);
+      digitalWrite (d6,Digit[10][5]);
+      digitalWrite (d7,Digit[10][6]);
+    return 0;}
   }
 
 void loop() {
